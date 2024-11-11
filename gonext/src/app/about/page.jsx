68fcc7a54c54e59268e78e35daf978854ea07b99
jmngandu <jamesmngandu@gmail.com>
2024-11-11 08:44:23 +0300

    go next

--- gonext/src/app/about/page.jsx
+++ gonext/src/app/about/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
 export default function about() {
+  const router = useRouter();
   return (
     <>
-      <h1>about section</h1>
+      <h1>about us</h1>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        go home
+      </button>
     </>
   );
 }
